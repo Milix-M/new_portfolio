954e626b3520abca4b9999e310663cd37dfda045
Milix-M <70957923+Milix-M@users.noreply.github.com>
2025-07-28 20:59:18 +0900

    Github Pages設定を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://profile.milix-m.com',
-  base: '/new_portfolio',
   vite: {
     ssr: {
       noExternal: ['react-icons']
